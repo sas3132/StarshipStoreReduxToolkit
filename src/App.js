ef730e077ece7158895a1350d9a4b1aef030e82c
Your Name <you@example.com>
2021-07-31 22:12:51 +0300

    Начало, Header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <header>
+                <div className="logoImg">
+                    <img src="/img/logo.png" alt="logo"/>
+                </div>
+                <div>
+                    <h1>StarshipStore</h1>
+                </div>
+                    <img src="/img/cart.svg" alt="cart"/>
+                    <span>499,999 cred.</span>
+            </header>
+            <div className="coverFalcon">
+                <img src="/img/cover.jpg" alt="buy"/>
+                <div className="centeredText">Buy now <span style={{color:'#D4E3EA'}}> Fly now</span></div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
