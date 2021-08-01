e2aae62631f77eb190be96e9816a316090d78f3a
Your Name <you@example.com>
2021-08-02 00:08:43 +0300

    grid сетка product

--- src/App.js
+++ src/App.js
@@ -11,10 +11,62 @@ function App() {
                     <img src="/img/cart.svg" alt="cart"/>
                     <span>499,999 cred.</span>
             </header>
+            <main className="main">
             <div className="coverFalcon">
                 <img src="/img/cover.jpg" alt="buy"/>
-                <div className="centeredText">Buy now <span style={{color:'#D4E3EA'}}> Fly now</span></div>
+                <div className="centeredText">Buy now <span style={{color:'#D4E3EA', whiteSpace:'nowrap'}}> Fly now</span></div>
             </div>
+            <section className="content">
+                <section className="contentProducts products">
+                    <div className="productsItems">
+                        <div className="productsItem">
+                            <div  className="productsImg">
+                                <img src="/img/productItem.jpg" alt=""/>
+                            </div>
+                            <div className="productName">Millennium Falcon</div>
+                            <div  className="productButton">
+                                <img src="/img/buttonBuy.svg" alt="buy"/>
+                            </div>
+
+                        </div>
+                        <div className="productsItem">
+                            <div  className="productsImg">
+                                <img src="/img/productItem.jpg" alt=""/>
+                            </div>
+                            <div className="productName">Millennium Falcon</div>
+                            <div  className="productButton">
+                                <img src="/img/buttonBuy.svg" alt="buy"/>
+                            </div>
+
+                        </div>
+                        <div className="productsItem">
+                            <div  className="productsImg">
+                                <img src="/img/productItem.jpg" alt=""/>
+                            </div>
+                            <div className="productName">Millennium Falcon</div>
+                            <div  className="productButton">
+                                <img src="/img/buttonBuy.svg" alt="buy"/>
+                            </div>
+
+                        </div>
+                        <div className="productsItem">
+                            <div  className="productsImg">
+                                <img src="/img/productItem.jpg" alt=""/>
+                            </div>
+                            <div className="productName">Millennium Falcon</div>
+                            <div  className="productButton">
+                                <img src="/img/buttonBuy.svg" alt="buy"/>
+                            </div>
+
+                        </div>
+                        </div>
+                </section>
+            </section>
+
+            </main>
+            <footer className="footer">
+                <div className="footerText">2021</div>
+            </footer>
         </div>
     );
 }
