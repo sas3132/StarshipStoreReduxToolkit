d110757754e8b238bbb34db3d26b0d26a99cf427
Your Name <you@example.com>
2021-08-06 20:10:05 +0300

    cart grid header padding

--- src/App.js
+++ src/App.js
@@ -1,51 +1,79 @@
+import {useState} from "react";
+
+
+const CartDrawer = () => {
+    return (
+        <div  className="cartDrawer">
+            <div className="cart">
+                <h2>Cart</h2>
+                <div className='items'>
+                    <div className="item">
+                        <img className="itemImg" src="/img/productItem.jpg" alt=""/>
+                        <section className="delNameItem">
+                            <p className="delNameItemP">Millennium Falcon</p>
+                            <img className="delItemCart" src="/img/delItemCart.svg" alt=""/>
+                        </section>
+                    </div>
+                    <div className="item">
+                        <img className="itemImg" src="/img/productItem3.jpg" alt=""/>
+                        <section className="delNameItem">
+                            <p className="delNameItemP">Twin Ion Engine Advanced x1</p>
+                            <img className="delItemCart" src="/img/delItemCart.svg" alt=""/>
+                        </section>
+                    </div>
+                    <div className="item">
+                        <img className="itemImg" src="/img/productItem4.jpeg" alt=""/>
+                        <section className="delNameItem">
+                            <p className="delNameItemP">J-type 327 Nubian royal starship</p>
+                            <img className="delItemCart" src="/img/delItemCart.svg" alt=""/>
+                        </section>
+                    </div>
+                </div>
+                <p className="totalCred">Total: 1,000,000,000 cred.</p>
+                <button className="buttonCheckout">Checkout</button>
+            </div>
+
+        </div>
+    );
+};
+
+
+
+
+
+
+
 
 
 function App() {
+
+
+    const [flag, setFlag] = useState(false)
+    const onToggle = () => setFlag(prev => !prev);
+
+
+
     return (
 
         <div className="wrapper clear">
 
-            <div style={{display: "none"}} className="cartDrawer">
-                <div className="cart">
-                    <h2>Cart</h2>
-                    <div className='items'>
-                        <div className="item">
-                            <img className="itemImg" src="/img/productItem.jpg" alt=""/>
-                            <section className="delNameItem">
-                                <p className="delNameItemP">Millennium Falcon</p>
-                                <img className="delItemCart" src="/img/delItemCart.svg" alt=""/>
-                            </section>
-                        </div>
-                        <div className="item">
-                            <img className="itemImg" src="/img/productItem3.jpg" alt=""/>
-                            <section className="delNameItem">
-                                <p className="delNameItemP">Twin Ion Engine Advanced x1</p>
-                                <img className="delItemCart" src="/img/delItemCart.svg" alt=""/>
-                            </section>
-                        </div>
-                        <div className="item">
-                            <img className="itemImg" src="/img/productItem4.jpeg" alt=""/>
-                            <section className="delNameItem">
-                                <p className="delNameItemP">J-type 327 Nubian royal starship</p>
-                                <img className="delItemCart" src="/img/delItemCart.svg" alt=""/>
-                            </section>
-                        </div>
-                    </div>
-                    <button className="buttonCheckout">Checkout</button>
-                </div>
+            {flag && < CartDrawer/>}
 
-            </div>
 
-            <header>
+            <header className="header">
+
                 <div className="logoImg">
                     <img src="/img/logo.png" alt="logo"/>
                 </div>
                 <div>
                     <h1>StarshipStore</h1>
                 </div>
-                <img className="imgCart" src="/img/cart.svg" alt="cart"/>
+                <img onClick={onToggle} className="imgCart" src="/img/cart.svg" alt="cart"/>
                 <span style={{whiteSpace: 'nowrap'}}>1,000,000,000 cred.</span>
+
             </header>
+
+
             <main className="main">
                 <div className="coverFalcon">
                     <img src="/img/cover.jpg" alt="buy"/>
@@ -53,6 +81,22 @@ function App() {
                     </div>
                 </div>
                 <div className="productsItems">
+                    <div className="productsItem">
+                        <div className="productsImg">
+                            <img src="/img/productItem4.jpeg" alt=""/>
+                        </div>
+                        <div className="productInfo">
+                            <b className="productName">J-type 327 Nubian royal starship</b>
+                            <p className="productDescription">Speed: 920km/h<br/>
+                                Class: Yacht<br/>
+                                Hyperdrive Rating: 1.8
+                            </p>
+                            <div className="productButton">
+                                <b>Cost: 499,999 credits</b>
+                                <img src="/img/buttonBuy.svg" alt="buy"/>
+                            </div>
+                        </div>
+                    </div>
                     <div className="productsItem">
                         <div className="productsImg">
                             <img src="/img/productItem.jpg" alt=""/>
@@ -101,26 +145,14 @@ function App() {
                             </div>
                         </div>
                     </div>
-                    <div className="productsItem">
-                        <div className="productsImg">
-                            <img src="/img/productItem4.jpeg" alt=""/>
-                        </div>
-                        <div className="productInfo">
-                            <b className="productName">J-type 327 Nubian royal starship</b>
-                            <p className="productDescription">Speed: 920km/h<br/>
-                                Class: Yacht<br/>
-                                Hyperdrive Rating: 1.8
-                            </p>
-                            <div className="productButton">
-                                <b>Cost: 499,999 credits</b>
-                                <img src="/img/buttonBuy.svg" alt="buy"/>
-                            </div>
-                        </div>
-                    </div>
+
 
 
                 </div>
             </main>
+
+
+
             <footer className="footer">
                 <div className="footerText">sas31 2021</div>
             </footer>
