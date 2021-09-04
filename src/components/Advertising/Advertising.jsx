09cae5dc5a4bc8e1940eb7d31ae68c115dc9f66f
sas3132 <78425379+sas3132@users.noreply.github.com>
2021-09-04 17:23:47 +0300

    Swiper add

--- src/components/Advertising/Advertising.jsx
+++ src/components/Advertising/Advertising.jsx
@@ -6,21 +6,29 @@ import {Swiper, SwiperSlide} from "swiper/react";
 
 
 import "swiper/swiper.scss";
-import "swiper/components/navigation/navigation.scss"
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/pagination/pagination.scss"
 
 
 
 import SwiperCore, {
+    Autoplay,
     Navigation,
+    Pagination,
 } from 'swiper';
 
-SwiperCore.use([Navigation]);
+SwiperCore.use([Autoplay,Navigation,Pagination]);
 
 
 const Advertising = () => {
     return (
 
-        <Swiper  className={styles.coverFalcon} loop={true} navigation={true} style={{marginLeft: 0,marginRight: 0, zIndex: 0}}>
+        <Swiper  className={styles.coverFalcon} pagination={{
+            "clickable": true
+        }} autoplay={{
+            "delay": 5000,
+            "disableOnInteraction": false
+        }}  loop={true} navigation={false} style={{'--swiper-navigation-color': '#861410',marginLeft: 0,marginRight: 0, zIndex: 0}}>
             <div className={styles.centeredText}>Buy now <span style={{color: '#D4E3EA', whiteSpace: 'nowrap'}}> Fly now</span>
                 </div>
                 <SwiperSlide >
